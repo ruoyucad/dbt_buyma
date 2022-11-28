2c384fbdf8b0f109141fe8f3d6f326e141cfe39e
RuoyuQian <ruoyucad@gmail.com>
2022-11-28 17:50:34 -0500

    init commit

---
+++ buyma/models/views/product_japanese.sql
@@ -0,0 +1,25 @@
+{{ config(materialized='table') }}
+
+with product_canada as (
+    select * from {{source('buyma', 'products_canada')}}
+),
+
+rates as (
+    select * from {{source('buyma', 'exchange_rates')}}
+),
+
+product_japanese as (
+select 
+    product_id,
+    brand,
+    product_name,
+    category,
+    price_cad * rates.rate as price_jpy,
+    product_url
+
+from product_canada
+left join rates
+    on rates.rate_id = product_canada.echange_rate_id
+)
+
+select * from product_japanese
