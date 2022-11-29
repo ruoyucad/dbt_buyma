bf6bf2bca5a33480b2e9c312b06838b04d620eb4
RuoyuQian <ruoyucad@gmail.com>
2022-11-28 20:22:27 -0500

    updated

--- models/views/latest_cadjpy.sql
+++ models/views/latest_cadjpy.sql
@@ -1,13 +1,5 @@
-with old_rates as (
-    select * from {{source('buyma', 'exchange_rates')}}
-),
+UPDATE  buyma_ruoyu.exchange_rates
 
-new_rate as (
-    select CADJPY from {{source('buyma', 'latest_cadjpy')}}
-)
-
-UPDATE old_rates
-
-SET rate = (select CADJPY from new_rate)
+SET rate = (select CADJPY from buyma_ruoyu.exchange_rates)
 
 WHERE currency = 'CADJPY'
