32e47e6f16c6d1d6cb3a66a9e554e4576a0aa91c
RuoyuQian <ruoyucad@gmail.com>
2022-11-28 21:40:16 -0500

    updatelogic

--- models/views/latest_cadjpy.sql
+++ models/views/latest_cadjpy.sql
@@ -1,7 +1,7 @@
 select 
     t1.*,
     case 
-        when t1.currency = 'CADJPY' then (select CADJPY from buyma_ruoyu.latest_cadjpy ) 
-    else t1.currency 
-    end as currency
+        when t1.currency = 'CADJPY' then t1.rate =  (select CADJPY from buyma_ruoyu.latest_cadjpy ) 
+    else 103 
+    end as rate
 from buyma_ruoyu.exchange_rates as t1
