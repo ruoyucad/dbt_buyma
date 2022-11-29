d1308f126584cf28e2fc1b6a0f53fe06215286b8
RuoyuQian <ruoyucad@gmail.com>
2022-11-28 21:38:01 -0500

    update

--- models/views/latest_cadjpy.sql
+++ models/views/latest_cadjpy.sql
@@ -1,7 +1,7 @@
 select 
     t1.*,
     case 
-        when t1.currency = 'CADJPY' then (select CADJPY from buyma_ruoyu.exchange_rates ) 
+        when t1.currency = 'CADJPY' then (select CADJPY from buyma_ruoyu.latest_cadjpy ) 
     else t1.currency 
     end as currency
-from {{ref('exchange_rates')}} as t1
+from buyma_ruoyu.exchange_rates as t1
