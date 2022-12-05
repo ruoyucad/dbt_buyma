ed03de4fedc62a74242885437ddd4ef81fcf520e
RuoyuQian <ruoyucad@gmail.com>
2022-12-04 19:38:26 -0500

    add safe_cast

--- models/views/guess_data_clean.sql
+++ models/views/guess_data_clean.sql
@@ -14,21 +14,33 @@ guess_data_transform as (
         'Guess' as brand,
         size,
         color,
-        LEFT(old_price, 1) as old_price,
-        LEFT(new_price, 1) as new_price,
-        old_price * rates.rate as old_price_jpy,
-        new_price * rates.rate as new_price_jpy,
+        ifnull(LEFT(old_price, 1),'0') as old_price,
+        ifnull(LEFT(new_price, 1),'0') as new_price,
         name,
         description,
         image_str,
         '1' AS echange_rate_id, 
 
     from guess_data
+),
+
+final as (
+    select 
+        id,
+        brand,
+        size,
+        color,
+        safe_cast(old_price as FLOAT64) * rates.rate as old_price_jpy,
+        safe_cast(new_price as FLOAT64) * rates.rate as new_price_jpy,
+        name,
+        description,
+        image_str,
+        current_timestamp as dbt_updated_at
+
+    from guess_data_transform
     left join rates
-    on rates.rate_id = guess_data.echange_rate_id
+    on rates.rate_id = guess_data_transform.echange_rate_id
 
-)
+) 
 
-
-select * from guess_data_transform
-
+select * from final
