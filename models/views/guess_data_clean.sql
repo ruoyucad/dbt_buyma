c08d833787b23f982f35d32170f2b296d3c0a280
RuoyuQian <ruoyucad@gmail.com>
2022-12-04 22:12:57 -0500

    add category

--- models/views/guess_data_clean.sql
+++ models/views/guess_data_clean.sql
@@ -25,11 +25,19 @@ guess_data_transform as (
     from guess_data
 ),
 
+
+ --https://www.guess.com/ca/en/men/apparel/jeans-and-denim/tapered/eco-distressed-tapered-jeans-blue/M2YAS2D4K32-FRMN.html
+-- https://www.guess.com/ca/en/men/apparel/jackets-and-coats/puffer-coats/arctic-patch-metallic-puffer-jacket-silver/M3RL50RCXP0-G9F9.html
+
 final as (
     select 
         id,
         url,
         brand,
+        CASE 
+            when SPLIT(url,'/')[OFFSET(6)] = 'features' then SPLIT(url,'/')[OFFSET(8)] 
+            when SPLIT(url,'/')[OFFSET(6)] = 'apparel' then SPLIT(url,'/')[OFFSET(7)] 
+        end as category,
         size,
         color,
         ROUND(safe_cast(old_price as FLOAT64) * rates.rate,1) as old_price_jpy,
@@ -43,7 +51,7 @@ final as (
     from guess_data_transform
     left join rates
     on rates.rate_id = guess_data_transform.echange_rate_id
-
+    where id <> 'GMDOLO-MBRLL'
 ) 
 
 select * from final
